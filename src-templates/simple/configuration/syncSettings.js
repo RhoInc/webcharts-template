ee26481f2f9ed952ed37a2308301b2c11e2151b7
jwildfire <jwildfire@gmail.com>
2019-12-14 12:19:00 -0500

    🌗

--- src-templates/simple/configuration/syncSettings.js
+++ src-templates/simple/configuration/syncSettings.js
@@ -2,6 +2,6 @@ export default function syncSettings(settings) {
     // webcharts settings
     settings.x.column = settings.x_col;
     settings.y.column = settings.y_col;
-    settings.marks[0].per[0] = settings.id_col;
+    settings.marks[0].per = [settings.id_col];
     return settings;
 }
